5b2d57dcaa05adc188faa12d941c8e9bbee5e217
samuel eze <samueleze1414@gmail.com>
2020-09-10 17:14:58 +0000

    facebook

--- app/src/main/java/com/example/facebook/MainActivity.java
+++ app/src/main/java/com/example/facebook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.facebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,13 +9,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 EditText email,pass;
 String Email,Pass;
 Button login;
+private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseAuth=FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
 login=findViewById(R.id.login);
 login.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +47,16 @@ validate();
             email.setError("please try again later");
         } else if (Pass.isEmpty()) {
             pass.setError("please your pin again");
-        }
+        }else{firebaseAuth.signInWithEmailAndPassword(Email,Pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Toast.makeText(MainActivity.this,"createUserWithEmail:onComplete"+task.isSuccessful(),Toast.LENGTH_LONG).show();
+                if (!task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this,"Authentication failed"+task.getException(),Toast.LENGTH_LONG).show();
+                }
+                }
+            });
+        };
 
 
     }
--- app/src/main/java/com/example/facebook/Registration.java
+++ app/src/main/java/com/example/facebook/Registration.java
@@ -1,24 +1,39 @@
 package com.example.facebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Registration extends AppCompatActivity {
  EditText email, surname, phone, password;
  String Email, Surname, Phone, Password;
  Button register, clear, registered;
-
+private FirebaseAuth firebaseAuth;
+private DatabaseReference databaseReference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+
         register=findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,11 +86,34 @@ public class Registration extends AppCompatActivity {
        password.setError("respect your old age");
 
    }else{
-       Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
-   }
+       firebaseAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(Registration.this, new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               Toast.makeText(Registration.this,"createUserWithEmail:onComplete"+task.isSuccessful(), Toast.LENGTH_LONG).show();
+             if(!task.isSuccessful()) {
+                 Toast.makeText(Registration.this,"Authentication failed"+task.getException(),Toast.LENGTH_LONG).show();
+             }else {
+                 FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+                 DatabaseReference  databaseReference=firebaseDatabase.getReference().child("user").push();
+                 FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+                 String userid=firebaseUser.getUid();
+                 databaseReference.child("Email").setValue(Email);
+                 databaseReference.child("Full name").setValue(surname);
+               databaseReference.child("Password").setValue(Password);
+               databaseReference.child("userid").setValue(userid);
+                 Intent intent =new Intent(Registration.this,MainActivity.class);
+                 startActivity(intent);
+                 finish();
+
+           }
+       }
 
 
-  }
+           });
+       }}}
 
 
-}
+
+
+
+
